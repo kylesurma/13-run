f5d668109d064348643663809ecb663ba93e3fe0
Kyle Surma <surmakyl@gmail.com>
2023-05-22 10:59:17 -0400

    - changed diamondbacks to D-backs  - removed unused documents

--- src/state/sanity.js
+++
@@ -1,8 +0,0 @@
-import { useSanityClient, groq } from 'astro-sanity';
-
-console.log(useSanityClient);
-
-export const getTeams = async () => {
-	const query = groq`*[_type == "team"]`;
-	return await useSanityClient().fetch(query);
-};
